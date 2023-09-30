c85477b7595f55c43009cd7e7a3007eff0b2eb5f
kylekhaitran <kylekhai04@gmail.com>
2023-09-30 16:15:49 -0400

    Add page transition for creating an account on Homepage
    Allows page to change to SelectAccount screen whe pressing CREATE ACCOUNT button on homepage

--- app/src/main/java/com/example/docappoint/Homepage.java
+++ app/src/main/java/com/example/docappoint/Homepage.java
@@ -2,8 +2,11 @@ package com.example.docappoint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -37,10 +40,23 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Homepage extends AppCompatActivity {
 
+    Button createAccountButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        // Connect UI elements to backend
+        createAccountButton = findViewById(R.id.createAccountButton);
+
+        //Handle click for the button
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), SelectAccount.class));
+            }
+        });
     }
 
 }
