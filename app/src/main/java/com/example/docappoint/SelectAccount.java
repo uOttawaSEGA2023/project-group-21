38d997b384a95e37190623d13046779adf54dca7
kylekhaitran <kylekhai04@gmail.com>
2023-09-30 17:07:30 -0400

    Add UI to new PatientRegister class and created DoctoeRegister class
    PatienRegister class will be shown if users on the SelectAccount screen chooses to create a Patient Account

--- app/src/main/java/com/example/docappoint/SelectAccount.java
+++ app/src/main/java/com/example/docappoint/SelectAccount.java
@@ -2,13 +2,28 @@ package com.example.docappoint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SelectAccount extends AppCompatActivity {
 
+    Button specifyPatientButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_account);
+
+        // Connect UI elements to backend
+       specifyPatientButton = findViewById(R.id.specifyPatientButton);
+
+        //Handle click for the button
+        specifyPatientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), PatientRegister.class));
+            }
+        });
     }
 }
