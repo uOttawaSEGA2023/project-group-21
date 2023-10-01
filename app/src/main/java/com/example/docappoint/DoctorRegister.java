91a9ab7f7b3fce48ff3f950858d445f01c0b9bdb
kylekhaitran <kylekhai04@gmail.com>
2023-09-30 21:49:32 -0400

    Add email and password integration with Firebase Auth for DoctorRegister
    - Firebase can now see the email and password field for Users signing up as doctor
    - DoctorHomepage class also created and it is the redirect target after successful authentication

--- app/src/main/java/com/example/docappoint/DoctorRegister.java
+++ app/src/main/java/com/example/docappoint/DoctorRegister.java
@@ -1,5 +1,6 @@
 package com.example.docappoint;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -11,12 +12,22 @@ import android.widget.EditText;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnFailureListener;
+
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.ktx.Firebase;
+
 public class DoctorRegister extends AppCompatActivity {
 
     // Declare variables to link to xml files
 
     EditText regDoctorFirstName, regDoctorLastName, regDoctorSpecialties, regDoctorAddress, regDoctorPhoneNumber, regDoctorEmail, regDoctorPassword, regDoctorConfirmPassword;
     Button createDoctorAccount, doctorBackToLogin;
+
+    // Add Firebase Integration (using Firebase Auth to query user)
+    FirebaseAuth dAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +46,8 @@ public class DoctorRegister extends AppCompatActivity {
         createDoctorAccount = findViewById(R.id.createDoctorAccountButton);
         doctorBackToLogin = findViewById(R.id.doctorBackToLoginButton);
 
+        // Initialize Firebase class
+        dAuth = FirebaseAuth.getInstance();
 
         // Click events
         createDoctorAccount.setOnClickListener(new View.OnClickListener() {
@@ -101,6 +114,27 @@ public class DoctorRegister extends AppCompatActivity {
 
                 // Success message if the account is successfully created (using Toast data)
                 Toast.makeText(DoctorRegister.this, "Success! Account Has Been Created!", Toast.LENGTH_SHORT).show();
+
+                // Create user when provided the email and password (if authentication is successful call addOnSuccessListener
+
+                dAuth.createUserWithEmailAndPassword(doctorEmail, doctorPassword)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+
+                            // Send the user to the doctor homepage
+                                startActivity(new Intent(getApplicationContext(),DoctorHomepage.class));
+
+                                // Remove all the previous activity
+                                finish();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(DoctorRegister.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
             }
         });
 
@@ -113,3 +147,4 @@ public class DoctorRegister extends AppCompatActivity {
         });
     }
 }
+
