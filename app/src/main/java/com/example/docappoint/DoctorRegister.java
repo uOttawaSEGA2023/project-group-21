bf8b600957313eca1b776d6e66a218d7a6a5c1c1
Kyle Khai Tran <kylekhai04@gmail.com>
2023-10-04 15:52:00 -0400

    Fix bugs

--- app/src/main/java/com/example/docappoint/DoctorRegister.java
+++ app/src/main/java/com/example/docappoint/DoctorRegister.java
@@ -17,6 +17,7 @@ import com.google.android.gms.tasks.OnFailureListener;
 
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
 import com.google.firebase.ktx.Firebase;
 
 public class DoctorRegister extends AppCompatActivity {
