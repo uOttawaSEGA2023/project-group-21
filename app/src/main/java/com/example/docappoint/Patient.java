9226da9e1955b732fef58b76071342d566ec4043
Kyle Khai Tran <kylekhai04@gmail.com>
2023-10-04 16:15:24 -0400

    Merge authentication-fb to main

---
+++ app/src/main/java/com/example/docappoint/Patient.java
@@ -0,0 +1,22 @@
+package com.example.docappoint;
+
+// Extend Patient with User
+public class Patient extends User {
+
+    // Unique attributes for Patient
+    private int healthCardNum;
+
+    // Constructor for Patient
+    public Patient(String firstName, String lastName, String email, String password, String confirmPassword, String phoneNumber, String
+            address, int healthCardNum) {
+        super(firstName, lastName, email, password, phoneNumber, address, confirmPassword);
+        this.healthCardNum = healthCardNum;
+    }
+
+    // Main method
+    public static void main(String[] args) {
+
+    }
+
+}
+
