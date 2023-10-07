0ea60fc347c59da28c6667bbd2516e5e03239d9d
Kyle Khai Tran <kylekhai04@gmail.com>
2023-10-07 18:00:58 -0400

    Add functionality for welcome homescreen and rough UI for all account homepages
    This redirects to the main Navigation screen for each account when the Continue button is pressed

--- app/src/main/java/com/example/docappoint/AdminHomepage.java
+++ app/src/main/java/com/example/docappoint/AdminHomepage.java
@@ -2,13 +2,29 @@ package com.example.docappoint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminHomepage extends AppCompatActivity {
+
+    Button adminContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_homepage);
+
+        //  Assign variables to direct xml files
+        adminContinue = findViewById(R.id.adminContinueButton);
+
+        // Continue button leading to doctor navigation page
+        adminContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), AdminNavigation.class));
+            }
+        });
     }
 }
