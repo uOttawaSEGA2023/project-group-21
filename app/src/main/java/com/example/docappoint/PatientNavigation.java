12669bdfe4b9e106bc903a5d4d1d07c32a029031
kylekhaitran <kylekhai04@gmail.com>
2023-10-07 19:45:37 -0400

    Add rough button to redirect to Settings page for PatientNavigation
    This is done to test the settings_xml file

--- app/src/main/java/com/example/docappoint/PatientNavigation.java
+++ app/src/main/java/com/example/docappoint/PatientNavigation.java
@@ -2,13 +2,29 @@ package com.example.docappoint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PatientNavigation extends AppCompatActivity {
+
+    Button patientSettingBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_navigation);
+
+        // Link UI to variable
+        patientSettingBtn = findViewById(R.id.patientSettingButton);
+
+        // Redirect to Patient settings screen when clicked
+        patientSettingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Settings.class));
+            }
+        });
     }
 }
