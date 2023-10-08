a2c46f7ab39164f78d29680466908e943e0106be
Kyle Khai Tran <kylekhai04@gmail.com>
2023-10-08 19:24:00 -0400

    Add fields from user databse to display on Settings page
    This lets Patient and Doctor accounts see their basic account information such as First Name, Last Name, Email and Phone Number.

--- app/src/main/java/com/example/docappoint/DoctorNavigation.java
+++ app/src/main/java/com/example/docappoint/DoctorNavigation.java
@@ -2,13 +2,29 @@ package com.example.docappoint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DoctorNavigation extends AppCompatActivity {
+
+    Button doctorSettingsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_navigation);
+
+        doctorSettingsBtn = findViewById(R.id.doctorSettingButton);
+
+        doctorSettingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Settings.class));
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/example/docappoint/Settings.java
+++ app/src/main/java/com/example/docappoint/Settings.java
@@ -1,14 +1,57 @@
 package com.example.docappoint;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.ktx.Firebase;
 
 public class Settings extends AppCompatActivity {
+
+    EditText nameAccountSettings, emailAccountSettings, phoneAccountSettings;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        // Initialize variables with UI
+        nameAccountSettings = findViewById(R.id.nameTextSettings);
+        emailAccountSettings = findViewById(R.id.emailTextSettings);
+        phoneAccountSettings = findViewById(R.id.phoneTextSettings);
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        userId = fAuth.getCurrentUser().getUid();
+
+        // Fetch data from Firestore database
+        DocumentReference documentReference = fStore.collection("Users").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                String firstName = documentSnapshot.getString("First Name");
+                String lastName = documentSnapshot.getString("Last Name");
+
+                // Concatenate the first name and last name with a space in between
+                String fullName = firstName + " " + lastName;
+                nameAccountSettings.setText(fullName);
+
+                emailAccountSettings.setText(documentSnapshot.getString("Email"));
+                phoneAccountSettings.setText(documentSnapshot.getString("Phone Number"));
+
+         }
+        });
+
     }
 }
