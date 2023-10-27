5f4e0c46f369cadb60d9af881773c330aee2fee2
Nahiyan Ishtiaque <nahiyan.ishtiaque@gmail.com>
2023-10-26 22:29:52 -0400

    implemented recycler view (just the ui)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ android {
 
 dependencies {
 
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
