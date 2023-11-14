624a855a3fb9fad885849b26e8ca429742736f84
Nahiyan Ishtiaque <nahiyan.ishtiaque@gmail.com>
2023-11-13 23:03:10 -0500

    update

--- app/src/main/java/com/example/docappoint/Doctor/DoctorApptHistory.java
+++ app/src/main/java/com/example/docappoint/Doctor/DoctorApptHistory.java
@@ -11,10 +11,24 @@ import com.example.docappoint.R;
 
 public class DoctorApptHistory extends AppCompatActivity {
 
+    Button doctorPastAppointmentsBackButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_appointment_history);
+
+        doctorPastAppointmentsBackButton = findViewById(R.id.doctorPastAppointmentsBackButton);
+
+        doctorPastAppointmentsBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(getApplicationContext(), DoctorNavigation.class));
+                finish();
+
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/docappoint/Doctor/DoctorNavigation.java
+++ app/src/main/java/com/example/docappoint/Doctor/DoctorNavigation.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import com.example.docappoint.R;
 import com.example.docappoint.Settings;
 
