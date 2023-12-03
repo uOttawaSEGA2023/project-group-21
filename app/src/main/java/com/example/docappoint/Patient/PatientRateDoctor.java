9ca09f98e8f43d4f82e6ea2e5c6c4fd2657b177a
Arun Sabaratnam <asaba059@uottawa.ca>
2023-12-02 20:19:09 -0500

    Added number aspect to rating

--- app/src/main/java/com/example/docappoint/Patient/PatientRateDoctor.java
+++ app/src/main/java/com/example/docappoint/Patient/PatientRateDoctor.java
@@ -4,14 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.example.docappoint.R;
 
 public class PatientRateDoctor extends AppCompatActivity {
 
-    Button patientRateDoctorBackBtn;
+    Button patientRateDoctorBackBtn, patientRateConfirmBtn;
+    EditText patientRateDoctorNum;
+    RatingBar patientRateRatingBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +27,41 @@ public class PatientRateDoctor extends AppCompatActivity {
 
         // Link xml files
         patientRateDoctorBackBtn = findViewById(R.id.patientRatingBackButton);
+        patientRateDoctorNum = findViewById(R.id.patientRatingNum);
+        patientRateRatingBar = findViewById(R.id.patientRatingBar);
+        patientRateConfirmBtn = findViewById(R.id.patientConfirmRatingButton);
+
+        addListeners();
+    }
+
+    private void addListeners(){
+        patientRateDoctorNum.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String num = s.toString();
+                if (!num.isEmpty()) {
+                    float rating = Float.parseFloat(num);
+                    patientRateRatingBar.setRating(rating);
+                }
+            }
+        });
+
+        patientRateRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                if (fromUser) {
+                    patientRateDoctorNum.setText(String.valueOf(rating));
+                }
+            }
+        });
 
         // Back Button
         patientRateDoctorBackBtn.setOnClickListener(new View.OnClickListener() {
@@ -28,5 +71,7 @@ public class PatientRateDoctor extends AppCompatActivity {
                 finish();
             }
         });
+
+
     }
 }
